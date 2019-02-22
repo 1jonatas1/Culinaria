5645d1bde7d84fae3b0dd0f7fae12a77d20573a4
1jonatas.santana1@gmail.com <1jonatas.santana1@gmail.com>
2019-02-22 08:36:47 -0300

    primeiro commit

---
+++ app/src/main/java/culinaria/example/dispositivosmoveis/culinaria/Culinaria.java
@@ -0,0 +1,13 @@
+package culinaria.example.dispositivosmoveis.culinaria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Culinaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_culinaria);
+    }
+}
